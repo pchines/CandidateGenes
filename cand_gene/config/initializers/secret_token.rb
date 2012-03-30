eba31103e62fd9da78c812ef8e717e5d79c0be5d
Peter Chines <pchines@gmail.com>
2012-03-30 10:35:42 +0000

    Standard Rails framework, generated with 'rails new cand_gene'

---
+++ cand_gene/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CandGene::Application.config.secret_token = 'ecd2117c85e2d1ec52c0051b8b6198808999880e9d7e63b5a73b2583411520536479d785aac56d7b3f86c460cb48e929daced7aad6d03e78dc2e25d06fe3d8ec'
