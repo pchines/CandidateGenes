92f10d23570228286a3b158146a442d7c6e336e2
Peter Chines <pchines@fusionprime.nhgri.nih.gov>
2012-05-30 06:00:35 -0400

    Add link from variant to UCSC browser

--- cand_gene1/app/models/variant.rb
+++ cand_gene1/app/models/variant.rb
@@ -3,6 +3,19 @@ class Variant < ActiveRecord::Base
   belongs_to :gene
   acts_as_versioned
 
+  def ucsc_link
+    p = self.key.split(':')
+    chrom = p[0]
+    start = p[1].to_i - 1000
+    if start < 0
+       start = 0
+    end
+    varend = p[2].to_i - 1
+    stop  = varend + 1000
+    url = "http://genome.cit.nih.gov/cgi-bin/hgTracks?db=#{self.genome}&position=#{chrom}:#{start}-#{stop}&hgct_customText=track+name=variant+visibility=pack+color=255,0,0%0a#{chrom}+#{p[1].to_i}+#{varend}+#{self.key}"
+    return url.html_safe
+  end
+
   @@validation_statuses = {
       0  => 'Not attempted',
       10 => 'Not valid',
