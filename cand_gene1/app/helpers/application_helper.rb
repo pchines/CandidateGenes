4a83a66a6d0ce77f09bee132c4f4f5f86f7d5da0
Peter Chines <pchines@fusionprime.nhgri.nih.gov>
2012-06-04 12:46:07 -0400

    Shorten long text in tables; allow HTML in show pages.  Define labels for nmi/dbsnp.

--- cand_gene1/app/helpers/application_helper.rb
+++ cand_gene1/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  @@MAX = 100
+
+  def short_text(text)
+    if (text.nil?)
+      return ""
+    end
+    short = text;
+    short.sub!('<(br|p)>.*', '...')
+    if short.length > @@MAX
+      short = short[0,@@MAX-3] + '...'
+    end
+    return short;
+  end
 end
