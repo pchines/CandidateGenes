fdee31de0ef020daf5a80654f617cb1a5258b41b
Peter Chines <pchines@fusionprime.nhgri.nih.gov>
2012-06-11 14:30:29 -0400

    Initial population of users table

--- cand_gene1/db/seeds.rb
+++ cand_gene1/db/seeds.rb
@@ -8,10 +8,20 @@
 
 lookups = Lookup.create([
   { item: 'Project name', value: '' },
-  { item: 'Offline', value: '' }
+  { item: 'Offline', value: '' },
+  { item: 'BackgroundColor', value '#DDD' },
   ])
 
 users = User.create([
   { username: 'lbonnyca', fullname: 'Lori Bonnycastle', is_admin: 1 },
   { username: 'pchines', fullname: 'Peter Chines', is_admin: 1 },
+  { username: 'fc', fullname: 'Francis Collins', is_admin: 0 },
+  { username: 'aswift', fullname: 'Amy Swift', is_admin: 0 },
+  { username: 'mikee', fullname: 'Mike Erdos', is_admin: 0 },
+  { username: 'morkenm', fullname: 'Mario Morken', is_admin: 0 },
+  { username: 'narisu', fullname: 'Narisu Narisu', is_admin: 0 },
+  { username: 'parkerst', fullname: 'Stephen Parker', is_admin: 0 },
+  { username: 'stitzelm', fullname: 'Michael Stitzel', is_admin: 0 },
+  { username: 'duboseaj', fullname: 'Amanda DuBose', is_admin: 0 },
+  { username: 'ramirezah', fullname: 'Andrea Ramirez', is_admin: 0 },
   ])
